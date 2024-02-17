0ed7c48281f29e118d3a15ce07a6ddf6c04f4546
Nelson Alves <jose.nelson.afonseca@gmail.com>
2024-02-17 11:41:25 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+
+    return (
+        <h1>
+            Welcome to the first react app of 2024!!!
+        </h1>
+
+    );
+
+}
+
+export default App;
