974a13ba755cb08b0b3a6d22289a4f0d2c7d46d9
Nelson Alves <jose.nelson.afonseca@gmail.com>
2025-02-10 10:54:53 -0800

    Added template of Plate2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React, {useEffect, useState} from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import withContainer from "./components/ContainerWrapper";
 import Plate01 from './plates/Plate01';
+import Plate02 from './plates/Plate02';
 import { getSource01 } from './util/data';
 
 const App = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        getSource01().then(setData);
-    }, []);
-
     return (
         <BrowserRouter>
             <Routes>
-                <Route index path="/" element={withContainer(<Plate01 data={data} />)} />
+                <Route index path="/plate01?" element={withContainer(<Plate01 />)} />
+                <Route index path="/plate02" element={withContainer(<Plate02 />)} />
             </Routes>
         </BrowserRouter>
     );
