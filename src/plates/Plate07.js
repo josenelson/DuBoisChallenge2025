2cafaff1d7aed3e3a67061115765309cab3736cf
Nelson Alves <jose.nelson.afonseca@gmail.com>
2025-03-17 08:47:32 -0700

    Added code to draw arcs

---
+++ src/plates/Plate07.js
@@ -0,0 +1,151 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { 
+    scaleLinear, 
+    line, 
+    extent, 
+    select,
+    curveNatural,
+    min,
+    max
+} from 'd3';
+import { getSource07 } from '../util/data';
+import Background from '../components/Background';
+import { ensureElement } from '../util/d3util';
+import { describeArc } from '../util/geometry';
+
+const margins = {
+    top: 20,
+    bottom: 20, 
+    left: 20,
+    right: 20
+}
+
+const titleText = "WIP Visualization";
+
+const TitleTextStyle = {
+    font: "2em 'B52-ULC W00 ULC'"
+}
+
+const getXRange = (size) => {
+    const leftMargin = margins.left;
+    
+    const xRange = [leftMargin, size.width - margins.right];
+
+    return xRange;
+}
+
+const getYRange = (size) => {
+    const titleTextElement = window.document.querySelector('#titleText');
+    const titleTextElementBox = titleTextElement.getBBox();
+
+    const topMargin = titleTextElementBox.y + titleTextElementBox.height + margins.top  ;
+
+    return [topMargin, size.height - (margins.bottom)];
+}
+
+const Visualization = ({
+    element, 
+    size,
+    data
+}) => {
+    if (data.length == 0) return;
+
+    // Data ranges
+    const value = d => d.value;
+    const year = d => d.year;
+    
+    const valueRange = extent(data, value);
+    const yearRange = extent(data, year);
+
+    const yRange = getYRange(size);
+    const xRange = getXRange(size);
+
+    const circleTotalSize = min([yRange[1] - yRange[0], xRange[1] - xRange[0]]);
+    const circleRadius = circleTotalSize / 2;
+    const circleCenter = [xRange[0] + ((xRange[1] - xRange[0]) / 2), yRange[0] + ((yRange[1] - yRange[0]) / 2)];
+    
+    // Scales
+
+    // Selections
+    const parentSelection = select(element);
+
+    let container = parentSelection.selectAll('g.container')
+                                     .data([data])
+                                     .join(
+                                        enter => {
+                                            const selection = enter.append('g')
+                                                                   .classed('container', true);
+                                            return selection;
+                                        }
+                                     );
+
+    const lineSelection = container.selectAll('path.mark')
+                                   .data([data])
+                                   .join(
+                                        enter => enter.append('path').classed('mark', true)
+                                   );
+
+    lineSelection.attr('d', (d, i) => {
+                        const path = describeArc({
+                            x: circleCenter[0],
+                            y: circleCenter[1],
+                            outerRadius: circleRadius,
+                            innerRadius: 100
+                        });
+
+                        return path;
+                  })
+                 .attr('stroke', 'black')
+                 .attr('fill', 'red')
+                 .attr('stroke-width', 2);
+};
+
+const Chart = ({
+    size
+}) => {
+    const containerRef = useRef(null);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getSource07();
+            
+            setData(data);
+        }
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (!containerRef.current) {
+            return;
+        }
+
+        Visualization({
+            element: containerRef.current, 
+            data: data,
+            size: size
+        });
+    }, [data, size]);
+
+    return (
+        <svg className='plate'>
+            <g>
+                <Background />
+                <text 
+                    id="titleText"
+                    style={TitleTextStyle}
+                    opacity="0.7"
+                    x={margins.left} 
+                    y={margins.top}>
+                    {titleText.split('\n').map((text, i) => (
+                        <tspan x={margins.left} dy="1.2em" key={i}>{text.toLocaleUpperCase()}</tspan>
+                    ))}
+                </text>
+                <g ref={containerRef} />
+            </g>
+        </svg>
+    );
+}
+
+export default Chart;
---
+++ src/util/geometry.js
@@ -0,0 +1,55 @@
+const polarToCartesian = (centerX, centerY, radius, angleInDegrees) => {
+	const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+	return {
+		x: centerX + (radius * Math.cos(angleInRadians)),
+		y: centerY + (radius * Math.sin(angleInRadians))
+	};
+}
+
+const describeArc = ({
+	x,
+	y,
+	outerRadius,
+	innerRadius,
+	angle = 220
+}) => {
+	/*
+	Arc command for reference:
+		A rx ry rotation large-arc-flag sweep-flag x y
+
+		rx, ry: The first two parameters are the imaginary circle’s horizontal and vertical radius. 
+				If we draw a circle, those two values are the same.
+
+		rotation: overall rotation in degress of the circle
+		large-arc-flag: should it go the long way to draw the arc of the short way
+		sweep-flag: reverses the arc to draw the other way
+		x, y: the end point of the arc
+	*/
+
+	const outerStartPoint = polarToCartesian(x, y, outerRadius, 0);
+	const outerEndPoint = polarToCartesian(x, y, outerRadius, angle);
+
+	const innerStartPoint = polarToCartesian(x, y, innerRadius, 0);
+	const innerEndPoint = polarToCartesian(x, y, innerRadius, angle);
+
+	const largeArcFlag = angle > 180 ? 1 : 0;
+
+	let d = [
+		 // Move to start point
+		`M ${outerStartPoint.x}, ${outerStartPoint.y}`,
+		// Draw the outer arc
+		`A ${outerRadius}, ${outerRadius}, 0, ${largeArcFlag}, 1, ${outerEndPoint.x}, ${outerEndPoint.y}`,
+		// Move to the inner arc end point
+		`L ${innerEndPoint.x} ${innerEndPoint.y}`,
+		// Draw the inner arc
+		`A ${innerRadius}, ${innerRadius}, 0, ${largeArcFlag}, 0, ${innerStartPoint.x}, ${innerStartPoint.y}`,
+		//`m 1 ${innerRadius - outerRadius}`, // Move to top point of inner radius
+		//`a ${outerRadius}, ${outerRadius}, 0, 1, 1, -1, 0`, // Close the inner ring. Actually will still work without, but inner ring will have one unit missing in strok
+		`Z`
+	];
+
+	return d.join(' ');
+}
+
+export { describeArc };
